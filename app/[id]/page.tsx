b8f33d5f0fc8192fd23a8527c9c1c43d921754fc
Sumer Singh <sumersm7@gmail.com>
2024-11-04 11:30:07 +0530

    save

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -9,10 +9,14 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { ArrowLeft } from "lucide-react";
 import { Navbar } from "@/components/navbar";
 import { PokemonInfoResponse } from "@/lib/types";
-type Params = Promise<{ id: string }>;
+import { headers } from "next/headers";
 
-export async function generateMetadata(props: { params: Params }): Promise<Metadata> {
-  const params = await props.params;
+import FlutterApp from "@/components/flutter";
+interface Props {
+  params: { id: string };
+}
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // const params = await props.params;
 
   if (params.id == undefined || params.id.includes(".")) {
     return {};
@@ -54,9 +58,21 @@ export async function generateMetadata(props: { params: Params }): Promise<Metad
   };
 }
 
-export default async function PostPage(props: { params: Params }) {
+export default async function Home({ params }: Props) {
+  const userAgent = headers().get("user-agent") || "";
+  const botPattern =
+    "(googlebot/|bot|Googlebot-Mobile|Mediumbot-MetaTagFetcher|Google-InspectionTool|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)";
+  const re = new RegExp(botPattern, "i");
+  const isBot = re.test(userAgent);
+  if (!isBot) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <FlutterApp />
+      </main>
+    );
+  }
   let pokemon: PokemonInfoResponse;
-  const params = await props.params;
+  // const params = await props.params;
   if (params.id == undefined || params.id.includes(".")) {
     return (
       <main className="min-h-screen bg-background">
